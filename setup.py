bcbd8cde6d2b8b09289c01fda7b2ca0352788745
Dubslow <bunslow@gmail.com>
2015-12-10 13:09:13 -0600

    Difficulties with PyPI uploading

--- setup.py
+++ setup.py
@@ -24,7 +24,7 @@ setup(
     # Versions should comply with PEP440.  For a discussion on single-sourcing
     # the version across setup.py and the project code, see
     # https://packaging.python.org/en/latest/single_source_version.html
-    version='0.0.1',
+    version='0.0.1.post2',
 
     description="A software 'suite' relating to aliquot sequences and their analysis",
     long_description=long_description,
