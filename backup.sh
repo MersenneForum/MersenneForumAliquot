d05b94cfe10a997814e6c01bd5ae53be284239c0
Christian Beer <christian.beer@posteo.de>
2017-11-04 12:41:08 +0100

    script to backup data files into git

---
+++ backup.sh
@@ -0,0 +1,8 @@
+#!/bin/bash
+
+webdir="/var/www/rechenkraft.net/aliquot/"
+
+cp $webdir/AllSeqs.txt website/html/
+cp $webdir/AllSeq.txt website/html/
+cp $webdir/AllSeq.json website/html/
+cp $webdir/statistics.json website/html/
