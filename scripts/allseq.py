0efd0884883e627311398a394d0175f32a5c1a53
Dubslow <bunslow@gmail.com>
2017-11-06 01:49:17 -0600

    To start with: all global variables in ALLCAPS

--- scripts/allseq.py
+++ scripts/allseq.py
@@ -24,29 +24,29 @@ import sys
 
 
 dir = '../website/html/'
-FILE = dir + 'AllSeq.html'
-TXT = dir + 'AllSeq.txt'
+ASHTML = dir + 'AllSeq.html'
+ASTXT = dir + 'AllSeq.txt'
 STATS = dir + 'statistics.html'
 JSON = dir + 'AllSeq.json'
 STATSON = dir + 'statistics.json'
-template = dir + 'template.html'
-template2 = dir + 'template2.html'
-seqfile = dir + 'AllSeqs.txt'
-statefile = sys.argv[0] + '.conf'
-lockfile = sys.argv[0] + '.lock'
-datefmt = '%Y-%m-%d %H:%M:%S'
+TMPLT = dir + 'template.html'
+STATSTMPLT = dir + 'template2.html'
+SEQLIST = dir + 'AllSeqs.txt'
+STATEFILE = sys.argv[0] + '.conf'
+LOCKFILE = sys.argv[0] + '.lock'
+DATEFMT = '%Y-%m-%d %H:%M:%S'
 
 
-reservation_page = 'http://www.mersenneforum.org/showpost.php'
-res_post_ids = (165249, 397318, 397319, 397320, 397912, 416583, 416585, 416586)
+RESPAGE = 'http://www.mersenneforum.org/showpost.php'
+RESPOSTIDS = (165249, 397318, 397319, 397320, 397912, 416583, 416585, 416586)
 
 
-per_hour = 55
-sleep_minutes = 60
-loop = False
-drop = []
-broken = {}
-#broken = {747720: (67, 1977171370480)}
+PERHOUR = 55
+SLEEPMINUTES = 60
+LOOPING = False
+TODROP = []
+BROKEN = {}
+#BROKEN = {747720: (67, 1977171370480)}
 # A dict of tuples of {broken seq: (offset, new_start_val)}
 
 ################################################################################
@@ -63,24 +63,25 @@ from mfaliquot.aliquot import get_guide, get_class, is_driver
 from mfaliquot.myutils import linecount, email, Print
 from mfaliquot.sequence import Sequence
 
-error_msg = ''
+ERRORMSG = ''
 
-composite = re.compile(r'= <a.+<font color="#002099">[0-9.]+</font></a><sub>&lt;(?P<C>[0-9]+)')
-smallfact = re.compile(r'(?:<font color="#000000">)([0-9^]+)(?:</font></a>)(?!<sub>)')
-largefact = re.compile(r'(?:<font color="#000000">[0-9^.]+</font></a><sub>&lt;)([0-9]+)')
-stuff = re.compile('<td bgcolor="#BBBBBB">n</td>\n<td bgcolor="#BBBBBB">Digits</td>\n<td bgcolor="#BBBBBB">Number</td>\n</tr><tr><td bgcolor="#DDDDDD">.{1,3}hecked</td>\n<td bgcolor="#DDDDDD">(?P<index>[0-9]+)</td>\n<td bgcolor="#DDDDDD">(?P<size>[0-9]+) <a href="index.php\\?showid=(?P<id>[0-9]+)">\\(show\\)')
-created = re.compile('([JFMASOND][a-z]{2,8}) ([0-9]{1,2}), ([0-9]{4})') # strftime('%d', strptime(month, "%B"))
+COMPOSITEREGEX = re.compile(r'= <a.+<font color="#002099">[0-9.]+</font></a><sub>&lt;(?P<C>[0-9]+)')
+SMALLFACTREGEX = re.compile(r'(?:<font color="#000000">)([0-9^]+)(?:</font></a>)(?!<sub>)')
+LARGEFACTREGEX = re.compile(r'(?:<font color="#000000">[0-9^.]+</font></a><sub>&lt;)([0-9]+)')
+INFOREGEX = re.compile('<td bgcolor="#BBBBBB">n</td>\n<td bgcolor="#BBBBBB">Digits</td>\n<td bgcolor="#BBBBBB">Number</td>\n</tr><tr><td bgcolor="#DDDDDD">.{1,3}hecked</td>\n<td bgcolor="#DDDDDD">(?P<index>[0-9]+)</td>\n<td bgcolor="#DDDDDD">(?P<size>[0-9]+) <a href="index.php\\?showid=(?P<id>[0-9]+)">\\(show\\)')
+CREATEDREGEX = re.compile('([JFMASOND][a-z]{2,8}) ([0-9]{1,2}), ([0-9]{4})') # strftime('%d', strptime(month, "%B"))
 #oldpage = re.compile('(<tr> <td>([0-9]+?)</td> <td>([0-9]+?)</td>.*?<td>)[0-9A-Za-z_ ]*?</td> </tr>') # Kept for historical purposes
 #oldjson = re.compile(r'(\[([0-9]+?), ([0-9]+?), ([0-9]+?), .*?)[0-9A-Za-z_ ]*?"\]') # Ditto
 
 
-quitting = False
-sleeping = False
+QUITTING = False
+SLEEPING = False
 def handler(sig, frame):
-     global quitting
-     quitting = True
-     if sleeping:
-          os.remove(lockfile)
+     Print(); Print("Recieved signal {}, now quitting".format(sig))
+     global QUITTING
+     QUITTING = True
+     if SLEEPING:
+          os.remove(LOCKFILE)
           sys.exit()
 signal.signal(signal.SIGTERM, handler)
 signal.signal(signal.SIGINT, handler)
@@ -89,25 +90,25 @@ signal.signal(signal.SIGINT, handler)
 def current_update(per_hour):
      this = []
      try:
-          with open(statefile, 'r') as conf: # Read which sequences to update
+          with open(STATEFILE, 'r') as conf: # Read which sequences to update
                start = int(conf.readline())
      except FileNotFoundError as e:
           Print('State file not found, starting from sequence 0')
           start = 0
      Print('Start:', start)
 
-     if drop: # Remove a sequence from the file
+     if TODROP: # Remove a sequence from the file
           seqs = []
-          with open(seqfile, 'r') as f:
+          with open(SEQLIST, 'r') as f:
                for line in f:
                     num = int(line)
-                    if num not in drop:
+                    if num not in TODROP:
                          seqs.append(num)
-          with open(seqfile, 'w') as f:
+          with open(SEQLIST, 'w') as f:
                for seq in seqs:
                     f.write(str(seq)+'\n')
 
-     with open(seqfile, 'r') as f:
+     with open(SEQLIST, 'r') as f:
           for i in range(start): # Skip to line number 'start'
                f.readline()
           for i in range(per_hour):
@@ -121,7 +122,7 @@ def current_update(per_hour):
 def get_reservations(pids):
      reserves = {}
      for pid in pids:
-          page = blogotubes(reservation_page + '?p={}&postcount=1'.format(str(pid)),
+          page = blogotubes(RESPAGE + '?p={}&postcount=1'.format(str(pid)),
                     hdrs={'User-Agent': 'Dubslow/AliquotSequences'})
           update = re.search(r'<!-- edit note -->.*Last fiddled with by [A-Za-z_0-9 -]+? on ([0-9a-zA-Z -]+) at <span class="time">([0-9:]{5})</span>', page, flags=re.DOTALL)
           updated = update.group(1)+' '+update.group(2)
@@ -181,22 +182,21 @@ def cofactor(s):
 
 
 def blogotubes(url, encoding='utf-8', hdrs=None):
-     global error_msg
+     global ERRORMSG, QUITTING
      if hdrs is None:
           hdrs = {'User-Agent': 'MersenneForum/Dubslow/AliquotSequences'}
-     global quitting
      req = request.Request(url, headers = hdrs )
      try:
           page = request.urlopen(req).read().decode(encoding)
      except error.HTTPError as e:
-          error_msg += 'HTTPError: '+str(e)+'\n'
+          ERRORMSG += 'HTTPError: '+str(e)+'\n'
           Print('HTTPError:', e)
-          quitting = True
+          QUITTING = True
           return None
      except Exception as e:
-          error_msg += 'Error! '+str(e)+'\n'
+          ERRORMSG += 'Error! '+str(e)+'\n'
           Print('Error!', e)
-          quitting = True
+          QUITTING = True
           return None
      else:
           return page
@@ -206,7 +206,7 @@ def id_created(i):
      i = str(i)
      #Print('Querying id', i)
      page = blogotubes('http://factordb.com/frame_moreinfo.php?id='+i)
-     date = created.search(page)
+     date = CREATEDREGEX.search(page)
      year = date.group(3)
      day = date.group(2)
      if len(day) == 1: day = '0'+day
@@ -222,9 +222,9 @@ def check(old, tries=3, reserves=None, special=None):
           return updateseq(old, reserves)
      # else:
      page = blogotubes('http://factordb.com/index.php?id='+str(old.id))
-     if quitting: return old
+     if QUITTING: return old
      if 'CF' in page: # Line unfactored, no progress since last update
-          old.time = strftime(datefmt, gmtime())
+          old.time = strftime(DATEFMT, gmtime())
           if isinstance(old.progress, int):
                old.progress = id_created(old.id)
           return old
@@ -235,26 +235,25 @@ def check(old, tries=3, reserves=None, special=None):
 
 
 def updateseq(old, reserves):
-     global error_msg
-     global quitting
+     global ERRORMSG, QUITTING
      tries = 5
-     if old.seq in broken:
-          seq = broken[old.seq][1]
+     if old.seq in BROKEN:
+          seq = BROKEN[old.seq][1]
           borked = True
      else:
           seq = old.seq
           borked = False
      while tries:
           page = blogotubes('http://factordb.com/sequences.php?se=1&action=last&aq='+str(seq))
-          if quitting: return old
+          if QUITTING: return old
           if 'Resources used by your IP' not in page:
-               info = stuff.search(page)
-               comps = composite.findall(page)
-               smalls = smallfact.findall(page)
-               bigs = largefact.findall(page)
+               info = INFOREGEX.search(page)
+               comps = COMPOSITEREGEX.findall(page)
+               smalls = SMALLFACTREGEX.findall(page)
+               bigs = LARGEFACTREGEX         .findall(page)
                if info: # Make sure we can actually get size/index info before in depth parsing
                     ali = Sequence(seq=seq, size=int(info.group('size')), index=int(info.group('index')), id=int(info.group('id')))
-                    ali.time = strftime(datefmt, gmtime())
+                    ali.time = strftime(DATEFMT, gmtime())
                     ali.res = reserves.get(seq, '')
                     if 'Not all factors known' in page:
                          factors = ''; size = 2
@@ -269,7 +268,7 @@ def updateseq(old, reserves):
                               tries -= 1
                               if tries == 0:
                                    Print('>'*10 + 'ERROR NO SMALL FACTORS')
-                                   error_msg += 'Seq {} had no smalls too many times\n'.format(seq)
+                                   ERRORMSG += 'Seq {} had no smalls too many times\n'.format(seq)
                                    return old
                               else:
                                    Print('Retrying ('+str(tries), 'tries left)')
@@ -293,7 +292,7 @@ def updateseq(old, reserves):
                               tries -= 1
                               if tries == 0:
                                    Print('>'*10 + 'ERROR NO COMPOSITES FOUND')
-                                   error_msg += 'Seq {} had no composite too many times\n'.format(seq)
+                                   ERRORMSG += 'Seq {} had no composite too many times\n'.format(seq)
                                    return old
                               else:
                                    Print('Retrying ('+str(tries), 'tries left)')
@@ -309,7 +308,7 @@ def updateseq(old, reserves):
                               tries -= 1
                               if tries == 0:
                                    Print('>'*10 + 'ERROR BAD SEQ MATCH')
-                                   error_msg += 'Seq {} had garbage values too many times\n'.format(seq)
+                                   ERRORMSG += 'Seq {} had garbage values too many times\n'.format(seq)
                                    return old
                               else:
                                    Print('Retrying ('+str(tries), 'tries left)')
@@ -320,7 +319,7 @@ def updateseq(old, reserves):
                               tries -= 1
                               if tries == 0:
                                    Print('>'*10 + 'ERROR NO COFACTOR')
-                                   error_msg += 'Seq {} had no cofactor\n'.format(seq)
+                                   ERRORMSG += 'Seq {} had no cofactor\n'.format(seq)
                                    return old
                               else:
                                    Print('Seq:', seq, 'no cofactor, retrying ({} tries left) factors: {}'.format(tries, factors))
@@ -330,7 +329,7 @@ def updateseq(old, reserves):
                               tries -= 1
                               if tries == 0:
                                    Print('>'*10 + 'ERROR SMALL COFACTOR')
-                                   error_msg += 'Seq {} had a small cofactor\n'.format(seq)
+                                   ERRORMSG += 'Seq {} had a small cofactor\n'.format(seq)
                                    return old
                               else:
                                    Print('Seq:', seq, 'small cofactor, retrying ('+str(tries), 'tries left) factors:', factors)
@@ -347,19 +346,19 @@ def updateseq(old, reserves):
                          if borked:
                               ali.seq = old.seq
                               ali.res = reserves.get(old.seq, '')
-                              ali.index += broken[old.seq][0]
-                              ali.progress += broken[old.seq][0]
+                              ali.index += BROKEN[old.seq][0]
+                              ali.progress += BROKEN[old.seq][0]
                          if ali.progress <= 0: ali.progress = id_created(ali.id)
                     else: # No 'Resources', no 'Not all factors known'
                          Print('Seq:', seq, 'Strange. Termination? More likely a bad sequence.')
                          ali.factors = "Reportedly terminated"
                          ali.guide, ali.clas, ali.driver = 'Terminated?', -5, True
                          ali.progress = 'Terminated?'
-                         error_msg += 'Supposedly, seq {} has terminated!!!'.format(seq)
+                         ERRORMSG += 'Supposedly, seq {} has terminated!!!'.format(seq)
                     return ali
           else: # Reached query limit
                Print('Seq:', seq, 'the DB is refusing requests.')
-               error_msg += 'Reached query or cpu limit. Oops.\n'
+               ERRORMSG += 'Reached query or cpu limit. Oops.\n'
                try:
                     # pages = re.search(r'>Page requests</td>\n<td[^>]*?>([0-9,]+)</td>', page).group(1)
                     # ^ avoid repeating the entire regex 5 times with slight variations. very typo prone.
@@ -373,26 +372,26 @@ def updateseq(old, reserves):
                     Print("{} page reqs, {} new ids, {} db queries, {} cputime since {}".format(pages, ids, queries, cputime, when))
                except AttributeError: # some re.search() failed
                     Print('Not only is it refusing requests, but its formatting has changed!')
-               quitting = True
+               QUITTING = True
                return old
 
 
 def inner_main(special=None):
-     global error_msg
-     print('\n'+strftime(datefmt))
-     total = linecount(seqfile)
+     global ERRORMSG
+     print('\n'+strftime(DATEFMT))
+     total = linecount(SEQLIST)
      if special:
           this = special
      else:
-          this, start = current_update(per_hour)
-     reserves, updated = get_reservations(res_post_ids)
-     data, oldinfo = get_old_info(JSON, reserves, this, drop)
+          this, start = current_update(PERHOUR)
+     reserves, updated = get_reservations(RESPOSTIDS)
+     data, oldinfo = get_old_info(JSON, reserves, this, TODROP)
      Print('Init complete, starting FDB queries')
 
      count = 0
      for old in oldinfo: # Loop over every sequence to be updated
           # never-before-checked sequences have index -1 (see get_old_info()) and if such a seq errors, there's no data here: ignore it.
-          if quitting:
+          if QUITTING:
                if old.index > 0:
                     data.append(old)
                continue
@@ -400,14 +399,14 @@ def inner_main(special=None):
           ali = check(old, reserves=reserves, special=special)
           if ali and ali.index > 0:
                data.append(ali)
-               if not quitting:
+               if not QUITTING:
                     count += 1
                     Print(count, 'sequences complete:', old.seq)
           sleep(1)
 
-     with open(template, 'r') as f: # Read in webpage templates
+     with open(TMPLT, 'r') as f: # Read in webpage templates
           html = f.read()
-     with open(template2, 'r') as f:
+     with open(STATSTMPLT, 'r') as f:
           stats = f.read()
 
      #dato = {ali.seq: ali for ali in data}
@@ -427,9 +426,9 @@ def inner_main(special=None):
                totprog += 1
 
      # Create broken sequences HTML
-     if broken:
+     if BROKEN:
           # horizontal table: create a list of tuples containing each column (i.e. each sequence)
-          entries = (('''<a href="http://factordb.com/sequences.php?se=1&aq={}&action=last20">{}</a>'''.format(broken[seq][1], seq), str(broken[seq][0])) for seq in sorted(broken))
+          entries = (('''<a href="http://factordb.com/sequences.php?se=1&aq={}&action=last20">{}</a>'''.format(BROKEN[seq][1], seq), str(BROKEN[seq][0])) for seq in sorted(BROKEN))
           row1, row2 = zip(*entries) # zip converts column data into row order
           r1 = ''.join('<td>{}</td>'.format(datum) for datum in row1)
           r2 = ''.join('<td>{}</td>'.format(datum) for datum in row2)
@@ -453,9 +452,9 @@ def inner_main(special=None):
      stats = stats.format(totinc=totlen/totsiz, avginc=avginc/total, totprog=totprog, progcent=totprog/total)
 
      # Write all the data and webpages
-     with open(FILE, 'w') as f:
+     with open(ASHTML, 'w') as f:
           f.write(html)
-     with open(TXT, 'w') as f:
+     with open(ASTXT, 'w') as f:
           f.write(txtdata)
      with open(STATS, 'w') as f:
           f.write(stats)
@@ -469,18 +468,18 @@ def inner_main(special=None):
           start += count
           if start >= total:
                start = 0
-          if count != per_hour and start != 0:
-               error_msg += 'Something went wrong. Only {} seqs were updated.\n'.format(count)
+          if count != PERHOUR and start != 0:
+               ERRORMSG += 'Something went wrong. Only {} seqs were updated.\n'.format(count)
           Print("Next start is", start)
-          with open(statefile, 'w') as conf: # Save how many sequences we updated
+          with open(STATEFILE, 'w') as conf: # Save how many sequences we updated
                conf.write(str(start)+'\n')
 
-     if error_msg:
+     if ERRORMSG:
           try:
-               email('Aliquot failure!', error_msg)
+               email('Aliquot failure!', ERRORMSG)
           except Exception as e:
                Print('Email failed:', e)
-               Print('Message:\n', error_msg)
+               Print('Message:\n', ERRORMSG)
 
      Print('Written HTML and saved state.')
 
@@ -489,40 +488,40 @@ def inner_main(special=None):
 # Start actual code execution
 
 def main():
-     global loop, sleeping
-     if os.path.exists(lockfile):
+     global LOOPING, SLEEPING
+     if os.path.exists(LOCKFILE):
           Print("Didn't start: lockfile is present")
           sys.exit(-1)
 
-     open(lockfile, 'a').close()
+     open(LOCKFILE, 'a').close()
 
      try:
           special = [int(arg) for arg in sys.argv[1:]]
      except ValueError:
           print('Error: Args are sequences to be run')
-          os.remove(lockfile)
+          os.remove(LOCKFILE)
           sys.exit(-1)
 
      if special:
-          loop = False
+          LOOPING = False
      else:
           special = None
 
      while True:
-     # This means you can start it once and leave it, but by setting loop = False you can make it one-and-done
+     # This means you can start it once and leave it, but by setting LOOPING = False you can make it one-and-done
      # This would be a good place for a do...while syntax
           try:
                inner_main(special)
           except Exception:
                raise # Errors are unhandled except to interrupt a sleeping loop, and to cleanup via finally
           finally:
-               os.remove(lockfile)
+               os.remove(LOCKFILE)
 
-          if loop and not quitting:
+          if LOOPING and not QUITTING:
                Print('Sleeping.')
-               sleeping = True
-               sleep(sleep_minutes*60)
-               sleeping = False
+               SLEEPING = True
+               sleep(SLEEPMINUTES*60)
+               SLEEPING = False
           else:
                break
 
