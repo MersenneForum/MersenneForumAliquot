73a4588280f3f320b2f3b4120382547ea3249168
Christian Beer <christian.beer@posteo.de>
2017-11-08 18:27:55 +0100

    small changes after update to newest upstream version

--- backup.sh
+++ backup.sh
@@ -2,7 +2,7 @@
 
 webdir="/var/www/rechenkraft.net/aliquot/"
 
-cp $webdir/AllSeqs.txt website/html/
+cp $webdir/SeqList.txt website/html/
 cp $webdir/AllSeq.txt website/html/
 cp $webdir/AllSeq.json website/html/
 cp $webdir/statistics.json website/html/
--- website/cron/cron-alimerge.sh
+++
@@ -1,2 +0,0 @@
-#!/bin/bash
-scl enable python33 /home/christianb/bin/alimerge.py >> /home/christianb/bin/log/alimerge.log 2>&1
