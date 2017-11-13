da7013abf6e2dd1ba5ef14e1e1db03b5fe5ca0bb
Dubslow <bunslow@gmail.com>
2017-11-13 03:37:59 -0600

    More reservations refactor work, much left TBD

--- mfaliquot/reservations.py
+++ mfaliquot/reservations.py
@@ -20,7 +20,7 @@
 '''
 
 import re
-from time import time, strftime, gmtime
+from time import time, gmtime, strptime, strftime
 
 from _import_hack import add_path_relative_to_script
 add_path_relative_to_script('..')
@@ -47,25 +47,34 @@ class AliquotReservations:
      identical to the MF posts in format, namely just a bunch of text lines
      whose format is specified by the `AliquotSequence.reservation_string`
      method. This class merely translates between a list of reslines and a
-     Python dict.'''
+     Python dict. Raises ValueErrors on problems, use str(exception) to get the
+     details.'''
+     # TODO: Reconsider if the datadict should be read-only, forcing callers
+     # to duplicate new reservations onto the datadict
+     # Pros: saves the caller duplicate work, IF the caller was using datadict
+     # allseq.py uses datadict but only reads reservations, reservations.py
+     # writes reservations but doesn't use datadict
 
      def __init__(self, seqinfo):
-          '''`data` is a reference to the overall data dictionary, to be used
-          as... well as a reference. It is not modified by this class.'''
+          '''`seqinfo` is a reference to the overall data dictionary, to be used
+          as a read-only... well, reference. It is not modified by this class.'''
           self._db = dict()
-          self._seqinfo = seqinfo
+          self._seqinfo = seqinfo #TODO: reconsider
+          self._when = None # timestamp written in file #TODO: way to set this
 
 
-     @staticmethod
-     def _read_res_line(line):
+     def _read_res_line(self, line):
           l = line.split()
           seq = AliquotSequence()
 
           try:
                seq.seq = int(l[0])
           except ValueError:
-               # TODO: check for timestamp and read it
-               return None
+               try:
+                    self._when = strptime(l, DATEFMT)
+               except ValueError:
+                    return
+
 
           try:
                seq.index = int(l[-2])
@@ -75,22 +84,20 @@ class AliquotReservations:
           else:
                seq.res = ' '.join(l[1:-2])
 
-          return seq
+          self._db[seq.seq] = seq
 
 
      @classmethod
-     def read_file(cls, file):
+     def read_file(cls, file, seqinfo):
           '''Read a file containing a list of
           `AliquotSequence.reservation_string` entires, ignoring any malformed
-          lines.'''
-          self = cls()
+          lines. seqinfo is a read-only reference to the overall data'''
+          self = cls(seqinfo)
           with open(file, 'r') as f:
                for line in f:
-                    seq = self._read_res_line(line)
-                    if seq:
-                         self._db[seq.seq] = seq
+                    self._read_res_line(line)
 
-          Print("Read {} seqs".format(len(self)))
+          Print("Read {} seqs".format(len(self._db)))
           return self
 
 
@@ -99,7 +106,7 @@ class AliquotReservations:
           format'''
           c = 0
           with open(file, 'w') as f:
-               f.write(strftime(DATEFMT, gmtime())+'\n')
+               f.write(strftime(DATEFMT, self._when)+'\n')
                for seq in sorted(db.keys()):
                     f.write(db[seq].reservation_string())
                     c += 1
@@ -113,47 +120,39 @@ class AliquotReservations:
                if seq in self._db:
                     other = self._db[seq].res
                     if name == other:
-                         raise ValueError(
-
-
-
-          raise ValueError("{} cannot reserve {}: does not exist".format(name, seq))
-
-
-
-
-
-
-
-
-
-
-
-
-def add_db(db, name, seqs):
-     global email_msg
-     for seq in seqs:
-          if seq in db:
-               other = db[seq].res
-               if name == other:
-                    string = "Warning: {} already owns {}".format(name, seq)
-                    Print(string)
-                    email_msg += string+'\n'
-               else:
-                    string = "Warning: seq {} is owned by {} but is trying to be reserved by {}!".format(seq, other, name)
-                    Print(string)
-                    email_msg += string+'\n'
-          else:
-               if info:
-                    infos = get_info(seq)
-                    if not infos:
-                         string = "Warning: {} doesn't appear to be in the list".format(seq)
-                         Print(string)
-                         email_msg += string+'\n'
+                         raise ValueError("Warning: {} already owns {}".format(name, seq))
                     else:
-                         db[seq] = AliquotSequence(seq=seq, res=name, index=infos[0], size=infos[1])
+                         raise ValueError("Warning: seq {} is owned by {} but is trying to be reserved by {}!".format(seq, other, name))
+               else:
+                    if seq not in self._seqinfo:
+                         raise ValueError("Warning: {} cannot reserve {}: does not exist".format(name, seq))
+                    info = self._seqinfo[seq]
+                    # When we read the res db from file, we don't record the other info, so leave it off for consistency
+                    self._db[seq] = AliquotSequence(seq=seq, res=name, index=info.index, size=info.size)
 
 
+     def unreserve_seqs(self, name, seqs):
+          '''Mark the `seqs` as no longer reserved. Raises ValueError on any
+          of the common problems.'''
+          b = c = len(seqs)
+          for seq in seqs:
+               try:
+                    exists = self._db[seq].res == name
+               except KeyError:
+                    raise ValueError("{} is not reserved at the moment ({})".format(seq, name))
+                    continue
+               if exists:
+                    del self._db[seq]
+                    c -= 1
+               else:
+                    raise ValueError("Warning: Seq {}'s reservation {} doesn't match dropee {}".format(seq, self._db[seq].res, name))
+
+          if c != 0:
+               raise ValueError("Only {} seqs were removed, {} were supposed to be dropped".format(b-c, b))
+
+
+     def apply_reservations_to_seqinfo(self):
+          #TODO
 
 
 
