01c611b161f568181acfb969afc3e337af7c3805
Dubslow <bunslow@gmail.com>
2017-11-18 18:33:33 -0600

    Progres commit

--- mfaliquot/application/seqdata.py
+++ mfaliquot/application/seqdata.py
@@ -19,23 +19,45 @@
 '''This is the common module for reading or writing to The Sequence Data
 File.'''
 
-#class NoWildcardImports:
-#     def __getitem__(self, index):
-#          raise ImportError("{} is not wildcard importable!".format(__name__))
-#__all__ = NoWildcardImports() # star imports are bad, mkay?
+
+from ..myutils import custom_inherit
+#from .heap import Heap
 
 
+################################################################################
+
+
+@custom_inherit(dict, delegator='_data', include=[
+'__len__', '__getitem__', '__contains__', '__get__', 'items', 'keys', 'values'])
 class SequenceData:
      '''The class that reads and writes The Sequence Data File. The `file`
-     constructor argument is immutable for the lifetime of the object.'''
+     constructor argument is immutable for the lifetime of the object. Writing
+     also writes to the other two files (which are read-only).'''
 
-     def __init__(self, jsonfile, txtfile):
+     def __init__(self, jsonfile, txtfile, resfile):
           '''Create the object with its one and only jsonfile. To switch files,
           you must finalize this object and "manually" move the file, then make
           a new SequenceData object.'''
-          self._jsonfile = file
+          # For priority purposes, we keep the jsonlist in minheap form ordered
+          # by priority. The dict is an access convenience for most purposes.
+          self._jsonfile = jsonfile
           self._txtfile = txtfile
+          self._resfile = resfile
           self._data = dict()
+          #self._heap = Heap()
+     # Here's the intended dataflow design: The dict is the master list of data,
+     # but the minheap/list is in charge of maintaining a (rough) heap order.
+     # When written to file, the data from the dict (being the master) is
+     # written, but in the order specified by the heap after that cycle's
+     # updates and other modifications. It doesn't even really matter if some of
+     # it isn't exactly a heap, because when reading, the data is used to
+     # initialize the dict, then the list is *sorted*. Since it was written to
+     # file in roughly a heap (and since the timsort used by Python is extremely
+     # efficient at leveraging any partial order already present in data), this
+     # initial sorting is nearly free. We then read the lowest N seqs off the
+     # list as this cycle's todo, and do any further updates in the cycle using
+     # the heap methods to maintain easy sortability with little cost. Then
+     # write, rinse and repeat.
 
 
      @property
@@ -46,23 +68,39 @@ class SequenceData:
      def read_from_file(self):
           '''Initialize self from the immutable `file` passed to the constructor.'''
           with open(self.file, 'r') as f:
-               olddat = json.load(f)['aaData']
-          for dat in olddat:
+               heap = json.load(f)['aaData']
+          self._heap = [None] * len(heap)
+          for i, dat in enumerate(heap):
                ali = AliquotSequence(lst=dat)
                self._data[ali.seq] = ali
+               #self._heap[i] = (ali.priority, ali.time, ali.seq)
+
+
+     def get_N_todo(self, N):
+          out, self._heap = self._heap[:N], self._heap[N:]
+          return out
 
 
      def write_to_file(self):
           '''Finalize self to the given file. Totally overwrites it with data
           from self.'''
-          ali_list = list(data_dict.values())
-          ali_list.sort(key=lambda ali: ali.seq)
-          # TODO: ali_list.sort(key=lambda ali: ali.priority)
 
-          json_string = json.dumps({"aaData": ali_list}).replace('],', '],\n')+'\n'
-          with open(self._file, 'w') as f:
+          # Find seqs that have been dropped from heap, they're just appended
+          # at the end
+          unsorted = set(self._data.keys()) - set(item[2] for item in self._heap)
+          out = [self._data[item[2]] for item in self._heap] # heap ali object may be out of date
+          out.extend(self._data[seq] for seq in unsorted)
+
+          json_string = json.dumps({"aaData": out}).replace('],', '],\n') + '\n'
+          txt_string = '\n'.join(str(ali) for ali in out) + '\n'
+          res_string = '\n'.join(ali.reservation_string() for ali in out) + '\n'
+
+          with open(self._jsonfile, 'w') as f:
                f.write(json_string)
 
-          txt_string = '\n'.join(str(ali) for ali in ali_list)
           with open(self._txtfile, 'w') as f:
-               f.write(txt_string+'\n')
+               f.write(txt_string)
+
+          with open(self._resfile, 'w') as f:
+               f.write(res_string)
+
