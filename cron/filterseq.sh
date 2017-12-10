49622fef84c60af92ea750f37b427a81ba3e3b5c
Christian Beer <christian.beer@posteo.de>
2017-12-10 13:07:25 +0100

    fix filterseq call
    forgot to copy from previous script

--- cron/filterseq.sh
+++ cron/filterseq.sh
@@ -1,6 +1,6 @@
 #!/bin/bash
 cd $HOME/MersenneForumAliquot/scripts
-scl enable rh-python36 "./filter_seqs.py" >> ../tmp/filterseq.txt
+scl enable rh-python36 "./filter_seqs.py \"isinstance(ali.cofact, int)\" \"(ali.cofact, ali.time)\" 50 \" \"" >> ../tmp/filterseq.txt
 
 scl enable rh-python36 "./allseq.py `cat ../tmp/filterseq.txt`" >> ../log/filterseq.log 2>&1
 
