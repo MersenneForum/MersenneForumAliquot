fa4ab5ea50ebfc9fd87fcf2f6cbe9746cbbd2b1e
Dubslow <bunslow@gmail.com>
2018-02-12 23:51:26 -0600

    Add verification sscript

---
+++ scripts/verify_terms.sh
@@ -0,0 +1,35 @@
+#    Copyright (C) 2014-2017 Bill Winslow
+#
+#    This script is a part of the mfaliquot package.
+#
+#    This program is libre software: you can redistribute it and/or modify
+#    it under the terms of the GNU General Public License as published by
+#    the Free Software Foundation, either version 3 of the License, or
+#    (at your option) any later version.
+#
+#    This program is distributed in the hope that it will be useful,
+#    but WITHOUT ANY WARRANTY; without even the implied warranty of
+#    MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.
+#
+#    See the LICENSE file for more details.
+
+aliqueit="/home/bill/aliqueit/aliqueit"
+termfile="./allseq.terms.txt"
+errfile="./error_seqs.txt"
+emailscript="/usr/bin/env python3 ./send_email.py"
+
+
+if [[ ! -s $termfile ]]; then exit 1; fi
+
+for seq in $(cat $termfile); do
+	wget "http://factordb.com/elf.php?seq=$seq&type=1" -O "alq_$seq.elf"
+	if $aliqueit -t $seq; then
+		rm "alq_$seq.elf"
+	else
+		echo "$seq" >> $errfile
+	fi
+done
+
+if [[ -s $errfile ]]; then
+	$emailscript "Sequences failed to verify as terminated: $(cat $errfile)"
+fi
