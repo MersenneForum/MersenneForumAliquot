47cd37eac0d9738c535f0b6ecc861849abd028f8
Dubslow <bunslow@gmail.com>
2018-02-26 13:37:44 -0600

    Actually fix the common index stuff, it's a but ugly
    tested for case where we drop 1 seq, multidrop merges testing pending

--- scripts/verify_merges.sh
+++ scripts/verify_merges.sh
@@ -30,6 +30,7 @@ fi
 for line in $(cat $mergefile); do
 	error=0
 	allelf=""
+	alltxt=""
 	# read $line into array $seqs using _ as delimiter
 	readarray -td_ seqs <<<"${line}_"; unset 'seqs[-1]';
 	for seq in "${seqs[@]}"; do
@@ -37,6 +38,7 @@ for line in $(cat $mergefile); do
 			wget "http://factordb.com/elf.php?seq=$seq&type=1" -O "alq_$seq.elf"
 		fi
 		allelf="${allelf} alq_$seq.elf"
+		alltxt="${alltxt} alq_$seq.txt"
 		if $aliqueit -t $seq; then
 			# cut away index number to determine common index value
 			cut -d . -f 2 alq_$seq.elf > alq_$seq.txt
@@ -48,11 +50,21 @@ for line in $(cat $mergefile); do
 	if [[ $error -gt 0 ]]; then
 		echo "$line" >> $errfile
 	else
+		# to find common lines between unsorted files, use grep -f -- the key is
+		# that the file proving the patterns must be excluded from files to be
+		# searched. Split alltxt between first and rest.
+		firsttxt=$(echo "$alltxt" | cut -d ' ' -f 1)
+		othertxt=$(echo "$alltxt" | cut -d ' ' -f 1 --complement)
+		# I'm pretty sure this is all totally silly, and it would be cleaner to
+		# construct onetxt/alltxt/allelf by using the $seqs array, but damned if I
+		# know how arrays work in bash, and this certainly works even if it's horribly
+		# inelegant
+
 		# determine common index (first line that is present in all alq_*.txt files)
-		ci=$(grep -F -h -f alq_${seqs[0]}.txt alq_*.txt | head -n1)
+		ci=$(grep -F -h -f $firsttxt $othertxt | head -n 1)
 		# find common index in all sequences and prepare for output
-		out=$(grep -F "$ci" $allelf)
-		$emailscript "Sequences verified as merged: $out"
+		out=$(grep -B 1 -F "$ci" $allelf)
+		$emailscript "$(echo -e "Sequences verified as merged:\n$out")"
 		# delete temporary files
 		for seq in "${seqs[@]}"; do
 			rm "alq_$seq.elf" "alq_$seq.txt"
