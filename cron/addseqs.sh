2843d32fc88fd315655ab4b70c933df3b38a5368
Christian Beer <christian.beer@posteo.de>
2018-08-23 20:08:47 +0200

    script to add new sequences
    Locking is happening in the python script nowadays. For now only add 50 sequences per call. The idea is that the script is called every 30 minutes.

---
+++ cron/addseqs.sh
@@ -0,0 +1,30 @@
+#!/bin/bash
+cd $HOME/MersenneForumAliquot/scripts
+
+if [ ! -e pointer ]; then
+  echo 0 > pointer
+fi
+
+input="../tmp/u2d_2e6.txt"
+last_pos=$(head -n 1 pointer)
+num_seqs=50
+
+newseqs=""
+
+cur_pos=0
+while [[ $num_seqs -gt 0 ]]; do
+  read -r alq
+  if [[ $cur_pos -lt $last_pos ]]; then
+    cur_pos=$((cur_pos+1))
+    continue
+  fi
+  newseqs="${newseqs} ${alq}"
+  num_seqs=$((num_seqs-1))
+  cur_pos=$((cur_pos+1))
+done < "$input"
+
+scl enable rh-python36 "./allseq.py $newseqs" > /dev/null 2>&1
+if [ $? -eq 0 ]; then
+  echo $cur_pos > pointer
+fi
+
