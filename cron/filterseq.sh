e1df3de3e74dffa99db349bfe686736a04d24dd8
Christian Beer <christian.beer@posteo.de>
2018-10-08 18:54:05 +0200

    jumpstart some of the smaller cofactors

--- cron/filterseq.sh
+++ cron/filterseq.sh
@@ -1,6 +1,6 @@
 #!/bin/bash
 cd $HOME/MersenneForumAliquot/scripts
-scl enable rh-python36 "./filter_seqs.py \"isinstance(ali.cofact, int)\" \"(ali.cofact, ali.time)\" 50 \" \"" >> ../tmp/filterseq.txt
+scl enable rh-python36 "./filter_seqs.py \"isinstance(ali.cofactor, int) and ali.priority < 6\" \"(ali.size, ali.cofactor)\" 100 \" \"" >> ../tmp/filterseq.txt
 
 scl enable rh-python36 "./allseq.py `cat ../tmp/filterseq.txt`" > /dev/null 2>&1
 
